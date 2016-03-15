545c4dbc31672d6b1ad50f668e495beb0d8a3584
정준영 <wnsdud1454@hanmail.net>
2016-03-15 12:56:58 +0900

    ex.01.py내용추가

--- ex.01/ex.01.py
+++ ex.01/ex.01.py
@@ -1,1 +1,7 @@
 print "hello world"
+print "hallo again"
+print "I like typing this."
+print "This is fun"
+print "Yayl Printing."
+print "I'd much rather you 'not'."
+print 'I "said" do not touch this.'
