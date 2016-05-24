1805205699d95f5ec58bc48219d55045b32e6cc0
JeongJunYoung <wnsdud1454@hanmail.net>
2016-05-24 10:55:52 +0900

    added ex34/ex34.py

---
+++ ex34/ex34.py
@@ -0,0 +1,12 @@
+# -*-coding:utf8
+
+animals = ['곰', '호랑이', '펭귄','얼룩말']
+bear = animals[0]
+tiger = animals[1]
+penguin = animals[2]
+zebra = animals[3]
+
+print("bear = %s" % bear)
+print("tiger = %s" % tiger)
+print("penguin = %s" % penguin)
+print("zebra = %s" % zebra)
